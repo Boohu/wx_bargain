563f911442045653ae80bdd058c78887a6f4383d
dragon <120267583@qq.com>
2016-12-05 20:09:10 +0800

    Update update.php

--- update.php
+++ update.php
@@ -5,5 +5,6 @@
  * Date: 2016/12/5 0005
  * Time: 下午 5:00
  */
-
-echo "<pre>".system("git fetch --all  & git reset --hard origin/master").</pre>;
+$ret= system("git fetch --all");
+$ret.="\n".system("git reset --hard origin/master");
+echo "<pre>".$ret.</pre>;
