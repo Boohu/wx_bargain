fff63de5faf340332b4ec1649185c5119c0e07e4
DoubleWei <544386107@qq.ocm>
2016-12-05 16:39:21 +0800

    新建插件

---
+++ site.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 砍价模块微站定义
+ *
+ * @author DoubleWei
+ * @url http://bbs.we7.cc/
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class Wx_bargainModuleSite extends WeModuleSite {
+
+	public function doMobileBargain() {
+		//这个操作被定义用来呈现 功能封面
+	}
+	public function doWebDetails() {
+		//这个操作被定义用来呈现 管理中心导航菜单
+	}
+	public function doWebOrder() {
+		//这个操作被定义用来呈现 管理中心导航菜单
+	}
+
+}
