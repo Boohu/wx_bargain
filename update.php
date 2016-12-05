be7be7e8dd8b106c8e916dc116cc59125473812d
dragon <120267583@qq.com>
2016-12-05 20:48:03 +0800

    Update update.php

--- update.php
+++ update.php
@@ -5,6 +5,10 @@
  * Date: 2016/12/5 0005
  * Time: 下午 5:00
  */
+
+ignore_user_abort();           // 即使Client断开(如关掉浏览器)，PHP脚本也可以继续执行.
+set_time_limit(0);             // 执行时间为无限制，php默认的执行时间是30秒，通过set_time_limit(0)可以让程序无
+
 $ret= system("git fetch --all");
 $ret.="\n".system("git reset --hard origin/master");
 echo "<pre>".$ret."</pre>";
