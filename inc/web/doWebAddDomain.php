5cf6084d0d98e4e43af6787eb320ed59be837084
120267583@qq.com <120267583@qq.com>
2016-12-06 17:47:10 +0800

    fix

--- inc/web/doWebAddDomain.php
+++ inc/web/doWebAddDomain.php
@@ -1,2 +1,14 @@
 <?php
-echo "1233333";
+require_once(dirname(__FILE__)."/../../model/domain.php");
+global $_GPC,$_W;
+
+$url=isset($_GPC['url'])?trim($_GPC['url']):"";
+if($url==""){
+    message('url不能为空');
+    exit;
+}
+if(Domain::add($url)){
+    message('操作成功');
+} else{
+    message('操作失败');
+}
--- inc/web/doWebDomain.php
+++ inc/web/doWebDomain.php
@@ -1,3 +1,4 @@
 <?php
 require_once(dirname(__FILE__)."/../../model/domain.php");
+$list = Domain::all();
 include $this->template("domain");
