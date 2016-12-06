a68b25c6ec4ff17233b3ffac6cf27efd260f0c24
dragon <120267583@qq.com>
2016-12-07 05:22:40 +0800

    Update doWebDelDomain.php

--- inc/web/doWebDelDomain.php
+++ inc/web/doWebDelDomain.php
@@ -1,1 +1,18 @@
 
+<?php
+require_once(dirname(__FILE__)."/../../model/domain.php");
+global $_GPC,$_W;
+
+$id=isset($_GPC['domain_id'])?intval($_GPC['domain_id']):"";
+
+var_dump($id);
+die();
+if($url==""){
+    message('url不能为空');
+    exit;
+}
+if(Domain::add($url)){
+    message('操作成功');
+} else{
+    message('操作失败');
+}
