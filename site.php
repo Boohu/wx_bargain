cf18d4546223891b7744dd08b1d53494c5afa943
120267583@qq.com <120267583@qq.com>
2016-12-07 09:36:04 +0800

    add mobile module

--- site.php
+++ site.php
@@ -20,7 +20,7 @@ class Wx_bargainModuleSite extends WeModuleSite {
 		//这个操作被定义用来呈现 管理中心导航菜单
 	}
 	public function doWebOrder() {
-    //这个操作被定义用来呈现 管理中心导航菜单
+	//这个操作被定义用来呈现 管理中心导航菜单
         require_once(dirname(__FILE__)."/inc/web/doWebOrder.php");
     }
     public function doWebManager() {
@@ -30,7 +30,7 @@ class Wx_bargainModuleSite extends WeModuleSite {
 
 
     public function doWebDomain(){
-        //多域名管理
+    //多域名管理
         require_once(dirname(__FILE__)."/inc/web/doWebDomain.php");
     }
 
@@ -45,13 +45,19 @@ class Wx_bargainModuleSite extends WeModuleSite {
     }
 
 
+    public function doMobileIndex(){
+    //活动主页
+        require_once(dirname(__FILE__)."/inc/mobile/doMobileIndex.php");
+
+    }
+
+
     /*移动端测试页面*/
     public function doMobileTest(){
         require_once  ( dirname(__FILE__)."/test/doMobileTest.php" );
     }
 
-
-    /*手机端测试页面*/
+    /*web端测试页面*/
     public function doWebTest(){
         require_once  ( dirname(__FILE__)."/test/doWebTest.php" );
     }
