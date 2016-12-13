d03c2ee396720f891615bb2df527328ea0bbda09
DoubleWei <544386107@qq.ocm>
2016-12-13 09:56:26 +0800

    修改信息填写样式

--- inc/mobile/doMobileInformation.php
+++ inc/mobile/doMobileInformation.php
@@ -11,9 +11,9 @@ $openid=$_W['openid'];//获得公众号ID
 $oid = $_GPC['oid'];//获得订单ID
 $order = OrderModel::getOrder($oid);//根据订单ID查询订单信息
 //非本人或非微信端点开信息填写连接
-if($openid!=$order[0]['openid']){
-  echo "错误";
-    exit;
-}
+//if($openid!=$order[0]['openid']){
+//  echo "错误";
+//    exit;
+//}
 
 include $this->template('information');
