2af2b3aece31b6599eec27abcd89d57f68fcef8d
DoubleWei <544386107@qq.ocm>
2016-12-13 19:23:58 +0800

    完善回复  修改活动新建

--- processor.php
+++ processor.php
@@ -16,25 +16,36 @@ class Wx_bargainModuleProcessor extends WeModuleProcessor
         global $_W;
         $content = $this->message['content'];//获取回复消息
         //这里定义此模块进行消息处理时的具体过程, 请查看微擎文档来编写你的代码
-        $op=preg_replace('/[\d]/u', '', $content);//获取要执行的操作
-        $id=preg_replace('/[^\d]/','',$content);//获取用户输入的编号
+        $op = preg_replace('/[\d]/u', '', $content);//获取要执行的操作
+        $id = preg_replace('/[^\d]/', '', $content);//获取用户输入的编号
         //判断回复内容
-        if ($op=="cj") {
+        if ($op == "cj") {
             $activity = ActiveModel::get($id);//获取活动信息
-            return $this->respNews(array(
-                'Title' => $activity[0]['name'],
-                'Description' => $activity[0]['desc'],
-                'PicUrl' => tomedia($activity[0]['head_pic']),
-                'Url' => $this->createMobileUrl('index', array('aid' => $id)),
-            ));
-        }elseif($op=="kj"){
-            $order=OrderModel::getOrder($id);
-            return $this->respNews(array(
-                'Title' => "订单编号".$order[0]['id'],
-                'Description' => 快点开链接帮你的小伙伴砍价吧,
-                'PicUrl' => tomedia('/preview.jpg'),
-                'Url' => $this->createMobileUrl('forward', array('oid' =>$id )),
-            ));
+            if (empty($activity)) {
+                $reply = "活动编号错误";
+                return $this->respText($reply);
+            } else {
+                return $this->respNews(array(
+                    'Title' => $activity[0]['name'],
+                    'Description' => $activity[0]['desc'],
+                    'PicUrl' => tomedia($activity[0]['head_pic']),
+                    'Url' => $this->createMobileUrl('index', array('aid' => $id)),
+                ));
+            }
+        } elseif ($op == "kj") {
+            $order = OrderModel::getOrder($id);
+            if (empty($order)) {
+                $reply = "订单编号错误";
+                return $this->respText($reply);
+
+            } else {
+                return $this->respNews(array(
+                    'Title' => "订单编号" . $order[0]['id'],
+                    'Description' => 快点开链接帮你的小伙伴砍价吧,
+                    'PicUrl' => tomedia('/preview.jpg'),
+                    'Url' => $this->createMobileUrl('forward', array('oid' => $id)),
+                ));
+            }
         }
     }
 }
