da3d937738a3c8601893a91058fdbc74b635adac
120267583@qq.com <120267583@qq.com>
2016-12-15 12:19:40 +0800

    时间控件修改

--- inc/web/doWebDetails.php
+++ inc/web/doWebDetails.php
@@ -1,4 +1,8 @@
 <?php
+require_once(dirname(__FILE__) . "/../../model/active.php");
+global $_GPC, $_W;
+$weid=$_W['uniacid'];    //获取公众号ID
 
+$datas= ActiveModel::get(null);
 
 include $this->template("details");
