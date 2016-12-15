4271522207f17aec4e61a5b8302f01301ef22dfc
120267583@qq.com <120267583@qq.com>
2016-12-15 13:50:16 +0800

    fix

--- inc/web/doWebDetails.php
+++ inc/web/doWebDetails.php
@@ -1,8 +1,18 @@
 <?php
 require_once(dirname(__FILE__) . "/../../model/active.php");
+require_once(dirname(__FILE__) . "/../../model/report.php");
+
 global $_GPC, $_W;
 $weid=$_W['uniacid'];    //获取公众号ID
 
 $datas= ActiveModel::get(null);
+
+foreach($datas as $key=>$val){
+    $id=$val['id'];
+    $datas[$key]['parNum']=repoModel::getParNum($id);
+    $datas[$key]['num']=repoModel::getNum($id);
+
+
+}
 
 include $this->template("details");
